d292359c98288c2d16c432aee6883769617aca8b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-09-20 16:49:58 -0700

    updated index.jsx to import React

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,7 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setmovies] = useState([
+  const [movies, setMovies] = useState([
     {
       id: 1,
       title: "The Godfather",
