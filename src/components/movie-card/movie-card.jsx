fd09617388863cabb99234044cc57877744623fd
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-10-04 11:42:05 -0700

    Updted movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
-  const imageURL = movie.ImageURL || 'default_image_url_here'; // Provide a fallback URL
+export const MovieCard = ({ movie }) => {
 
   return (
-    <div onClick={() => onMovieClick(movie)}>
-      <img src={imageURL} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.directors}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    ImageURL: PropTypes.string, // No longer required
-    Title: PropTypes.string,
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+      title: PropTypes.string.isRequired,
+      imagePath: PropTypes.string.isRequired,
+      director: PropTypes.object.isRequired
+  }).isRequired
 };
