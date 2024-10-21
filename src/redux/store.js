53528ac2eb245dba957e9568b3b1ad8d9a36ef18
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-10-21 00:53:22 -0700

    updated to fix issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
-import { configureStore } from "@redux.js/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./reducers/movies";
-import userReducer from "./reducers/user";
+import userReducer from "./reducers/user/user";  // Correct path
 
 export const store = configureStore({
-  reducer: { movies: moviesReducer, user: userReducer },
+  reducer: {
+    movies: moviesReducer,
+    user: userReducer,
+  },
 });
