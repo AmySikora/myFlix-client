1133d52e2f09f6c5adcaa763991d4856268d3a1b
AmySikora <101671204+AmySikora@users.noreply.github.com>
2024-10-20 21:45:16 -0700

    updated typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@redux.js/toolkit";
 import moviesReducer from "./reducers/movies";
-import userReducer from "./reducers/user/user";
+import userReducer from "./reducers/user";
 
 
 export const store = configureStore({
