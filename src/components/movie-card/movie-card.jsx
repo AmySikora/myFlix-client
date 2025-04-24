b55395ebbde9607f3fb67aeccad5de033b6d03c1
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-04-24 12:55:54 -0700

    updated to make movies clickable

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,56 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./movie-card.scss"; // make sure this is included
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie }) => {
-  const directorName = typeof movie.director === "object" ? movie.director?.Name : movie.director;
+  const directorName =
+    typeof movie.director === "object" ? movie.director?.Name : movie.director;
 
   const truncate = (text, maxLength) =>
     text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
 
   return (
-    <Card className="favorite-movie-card">
-    <Link to={`/movies/${movie.id}`} className="text-decoration-none" state={{ from: "main" }}>
-      <Card.Img variant="top" src={movie.image} className="favorite-movie-image" />
+    <Link
+      to={`/movies/${movie.id}`}
+      state={{ from: "main" }}
+      className="movie-card-link"
+    >
+      <Card className="favorite-movie-card">
+        <Card.Img variant="top" src={movie.image} className="favorite-movie-image" />
+        <Card.Body className="d-flex flex-column justify-content-between">
+          <div>
+            <Card.Title className="movie-title">{truncate(movie.title, 30)}</Card.Title>
+            <Card.Text className="movie-description">
+              {truncate(movie.description, 100)}
+            </Card.Text>
+          </div>
+          {directorName && (
+            <Card.Text className="movie-director">
+              <strong>Director:</strong> {directorName}
+            </Card.Text>
+          )}
+          <div className="btn btn-primary mt-2 w-100">Open</div>
+        </Card.Body>
+      </Card>
     </Link>
-  
-    <div className="favorite-movie-info">
-      <div>
-        <h5 className="movie-title">{truncate(movie.title, 30)}</h5>
-        <p className="movie-description">{truncate(movie.description, 100)}</p>
-      </div>
-      {directorName && (
-        <p className="movie-director">
-          <strong>Director:</strong> {directorName}
-        </p>
-      )}
-      <Button
-        as={Link}
-        to={`/movies/${movie.id}`}
-        state={{ from: "main" }}
-        className="btn mt-2"
-      >
-        Open
-      </Button>
-    </div>
-  </Card>  
   );
-};
-
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    director: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.shape({
-        Name: PropTypes.string,
-      }),
-    ]),
-    genre: PropTypes.string,
-  }).isRequired,
 };
