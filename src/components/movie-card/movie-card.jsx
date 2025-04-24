2ace332de8e33bb52d71b82f56f5de889bc68a6c
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-04-24 13:06:34 -0700

    UPdated movie card for style consistencies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -18,22 +18,45 @@ export const MovieCard = ({ movie }) => {
       className="movie-card-link"
     >
       <Card className="favorite-movie-card">
-        <Card.Img variant="top" src={movie.image} className="favorite-movie-image" />
-        <Card.Body className="d-flex flex-column justify-content-between">
-          <div>
-            <Card.Title className="movie-title">{truncate(movie.title, 30)}</Card.Title>
+        <Card.Img
+          variant="top"
+          src={movie.image}
+          className="favorite-movie-image"
+          alt={movie.title}
+        />
+        <Card.Body className="d-flex flex-column justify-content-between flex-grow-1">
+          <div className="flex-grow-1">
+            <Card.Title className="movie-title">
+              {truncate(movie.title, 30)}
+            </Card.Title>
             <Card.Text className="movie-description">
               {truncate(movie.description, 100)}
             </Card.Text>
           </div>
           {directorName && (
-            <Card.Text className="movie-director">
+            <Card.Text className="movie-director mt-2">
               <strong>Director:</strong> {directorName}
             </Card.Text>
           )}
-          <div className="btn btn-primary mt-2 w-100">Open</div>
+          <div className="btn-favorite-card mt-3 text-center">Open</div>
         </Card.Body>
       </Card>
     </Link>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        Name: PropTypes.string,
+      }),
+    ]),
+    genre: PropTypes.string,
+  }).isRequired,
 };
