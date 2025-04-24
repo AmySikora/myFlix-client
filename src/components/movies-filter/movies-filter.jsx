c2369d81c6e6c8c3c7fe1e2930b7874a5e11f9e9
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-04-23 21:11:05 -0700

    updated search bar to be smaller

--- src/components/movies-filter/movies-filter.jsx
+++ src/components/movies-filter/movies-filter.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Form from "react-bootstrap/Form";
 import { setFilter, resetFilter } from "../../redux/reducers/movies";
+import { useNavigate } from "react-router-dom";
 
 export const MoviesFilter = () => {
   const filter = useSelector((state) => state.movies.filter);
+  const movies = useSelector((state) => state.movies.list);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleClearFilter = () => {
-    dispatch(resetFilter()); 
-  };
+  // Optional: Auto-redirect if 1 match is left after typing
+  useEffect(() => {
+    const filtered = movies.filter((movie) => {
+      const searchText = filter.toLowerCase();
+      return (
+        movie.title.toLowerCase().includes(searchText) ||
+        movie.director?.toLowerCase().includes(searchText) ||
+        movie.genre?.toLowerCase().includes(searchText)
+      );
+    });
+
+    if (filtered.length === 1) {
+      dispatch(resetFilter());
+      navigate(`/movies/${filtered[0].id}`, { state: { from: "main" } });
+    }
+  }, [filter, movies, dispatch, navigate]);
 
   return (
     <Form.Control
