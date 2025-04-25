a3af78a16e82aa9075717f356acabb111ba95b9a
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-04-24 17:45:36 -0700

    updated scss

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -52,10 +52,4 @@ MovieCard.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    director: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.shape({ Name: PropTypes.string }),
-    ]),
-  }).isRequired,
-};
+    
