edce1be47b0277f34509c8faeec973c7cc990d34
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-04-24 18:19:58 -0700

    updated movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "react";;
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./movie-card.scss";
